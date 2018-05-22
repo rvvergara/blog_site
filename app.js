46ef684e049bd809f8b3b4130fe0c79ea72f89ae
Ryan <rvvergara@gmail.com>
2018-05-22 16:14:51 +0800

    Include create new comment route & edit comments/new.ejs

--- app.js
+++ app.js
@@ -88,7 +88,29 @@ app.delete("/posts/:id",function(req,res){
 });
 
 // 8. NEW COMMENTS - /posts/:id/comments/new - GET - Show comment form for specific post - Post.findByID(req.params.id,callback(err,foundPost)) 
+app.get("/posts/:id/comments/new",function(req,res){
+    Post.findById(req.params.id,function(err,foundPost){
+        if(err) console.log(err)
+        else res.render("comments/new",{post:foundPost});    
+    });
+});
 
+// 9. CREATE COMMENT - /posts/:id - POST - Create comment and redirect to post page - Post.findById(req.params.id,callback(err,foundPost))
+app.post("/posts/:id",function(req,res){
+    Post.findById(req.params.id,function(err,foundPost){
+        if(err) console.log(err)
+        else{
+            Comment.create(req.body.comment,function(err,newComment){
+                if(err) console.log(err)
+                else{
+                    foundPost.comments.push(newComment);
+                    foundPost.save();
+                    res.redirect("/posts/"+foundPost.id);
+                }
+            });
+        }
+    });
+});
 
 //Turning on Node server
 app.listen(7500,function(){
